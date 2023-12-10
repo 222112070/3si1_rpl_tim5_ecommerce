dd1cc6d52181bee94140364ccdd0d66d9eb44185
Himawan Wahid Ikhwansyah <himawanwahid12@gmail.com>
2023-12-10 17:23:05 +0700

    Data Initializer

--- src/main/java/com/kel5/ecommerce/repository/RoleRepository.java
+++ src/main/java/com/kel5/ecommerce/repository/RoleRepository.java
@@ -8,8 +8,11 @@ import com.kel5.ecommerce.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
+    Optional<Role> findRoleByName(String name);
 }
